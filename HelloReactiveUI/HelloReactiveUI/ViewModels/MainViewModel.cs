46baab29ce1fa7f521fa5525978ad0679c8603d1
mallibone <mallibone@outlook.com>
2018-07-31 13:36:03 +0200

    Initial migration to .Net Standard

---
+++ HelloReactiveUI/HelloReactiveUI/ViewModels/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Input;
+using ReactiveUI;
+
+namespace HelloNetStandard.ViewModels
+{
+    class MainViewModel : ReactiveObject
+    {
+        private int _clickCount = 0;
+        private string _buttonText;
+
+        public MainViewModel()
+        {
+            ButtonSelectedCommand = ReactiveCommand.Create(() => { ButtonText = $"Clicked {++_clickCount} times"; });
+
+            ButtonText = "Click me";
+        }
+
+        public string ButtonText
+        {
+            get => _buttonText;
+            set => this.RaiseAndSetIfChanged(ref _buttonText, value);
+        }
+
+        public ICommand ButtonSelectedCommand { get; }
+    }
+}
